b5b694347c1f5c6335380bb61087e83b92564151
Fengtong He <fengtonghe@gmail.com>
2022-10-07 16:00:35 -0500

    Generate controller Manufactures

---
+++ app/controllers/manufactures_controller.rb
@@ -0,0 +1,4 @@
+class ManufacturesController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'manufacture/index'
+  get "/manufactures", to: "manufactures#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
