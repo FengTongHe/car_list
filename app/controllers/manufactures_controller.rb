a12aef0cd6845402fd164b8a382b9d7ceec7937d
Fengtong He <fengtonghe@gmail.com>
2022-10-08 17:22:35 -0500

    adding manufacture pages

--- app/controllers/manufactures_controller.rb
+++ app/controllers/manufactures_controller.rb
@@ -1,7 +1,9 @@
 class ManufacturesController < ApplicationController
   def index
+    @manufactures = Manufacture.all.order("name ASC")
   end
 
   def show
+    @manufacture = Manufacture.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'manufactures/index'
-  get 'manufactures/show'
+  # get "manufactures/index"
+  # get "manufactures/show"
   root "manufactures#index"
 
   get "/manufactures", to: "manufactures#index"
+  get "/manufactures/:id", to: "manufactures#show", as: "manufacture", constraints: { id: /\d+/ }
 end
