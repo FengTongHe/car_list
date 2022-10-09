6274dbcade603fa22865159b7529c540a9f675bd
Fengtong He <fengtonghe@gmail.com>
2022-10-08 23:43:42 -0500

    generated controller Models

--- app/controllers/years_controller.rb
+++ app/controllers/years_controller.rb
@@ -1,6 +1,6 @@
 class YearsController < ApplicationController
   def index
-    @years = Year.includes(:manufacture).order("year DESC")
+    @years = Year.includes(:manufacture).all.order("year DESC")
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'models/index'
+  get 'models/show'
   root "manufactures#index"
 
   get "/manufactures", to: "manufactures#index"
   get "/manufactures/:id", to: "manufactures#show", as: "manufacture", constraints: { id: /\d+/ }
 
-  get "years/index", to: "years#index"
-  get "years/show", to: "years#show", as: "year", constraints: { id: /\d+/ }
+  get "years/index"
+  get "years/show"
+
+  resources :years, only: %i[index show]
 end
