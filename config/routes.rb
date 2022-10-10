506c17528274af9a954e43b8569fcf1f94bfc1bf
Fengtong He <fengtonghe@gmail.com>
2022-10-10 17:57:27 -0500

    Header and navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   root "manufactures#index"
 
-  get "/manufactures", to: "manufactures#index"
-  get "/manufactures/:id", to: "manufactures#show", as: "manufacture", constraints: { id: /\d+/ }
+  get "/manufactures/index"
+  get "/manufactures/show"
+  resources :manufactures, only: %i[index show]
 
   get "years/index"
   get "years/show"
   resources :years, only: %i[index show]
 
-  get 'models/index'
-  get 'models/show'
+  get "models/index"
+  get "models/show"
   resources :models, only: %i[index show]
 end
